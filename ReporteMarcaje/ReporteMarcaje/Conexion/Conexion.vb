11ce6c8e85297a0cccbb6725b15fbd6e3ccda045
ctapiacortes <cristian.tc89@gmail.com>
2014-10-23 16:43:54 -0300

    Creadas conexion y variables globales

---
+++ ReporteMarcaje/ReporteMarcaje/Conexion/Conexion.vb
@@ -0,0 +1,21 @@
+﻿Public Class Conexion
+    Public Sub abrir(ByVal c As SqlClient.SqlConnection)
+        Try
+            c.ConnectionString = "Data Source={SQL Server};server='" & servidorBD & "';database='" & "CuboGestion" & "';uid='" & usuarioBD & "';pwd='" & claveBD & "'"
+            c.Open()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Public Sub cerrar(ByVal c As SqlClient.SqlConnection)
+        Try
+            c.Close()
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+
+End Class
---
+++ ReporteMarcaje/ReporteMarcaje/Conexion/Publica.vb
@@ -0,0 +1,7 @@
+﻿Module Publica
+    Public servidorDB As String = "NT-AEPBIO"
+    Public usuarioBD As String = "sa"
+    Public claveBD As String = "1453Bizancio"
+
+
+End Module
